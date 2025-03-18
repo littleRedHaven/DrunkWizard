cbe1a4cca18e39aa04cc998239f2ece12548fe60
littleRedHaven <morgymoo2005@gmail.com>
2025-03-17 22:19:17 -0500

    voice recognition attempt
    I think it is all set up except for the activation which Im not sure if its not working because of the headset emulator

---
+++ My project/Assets/CollisionDetector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+
+    public 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ My project/Assets/speechDetection.cs
@@ -0,0 +1,47 @@
+using Meta.WitAi;
+using Oculus.Voice;
+using System.Collections; 
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class speechDetection : MonoBehaviour
+{
+    public AppVoiceExperience wit;
+    public GameObject textmesh;
+    public Collision cauldCollis;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cauldCollis = GetComponent<Collision>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+
+            wit.Activate();
+            Debug.Log("Pressed");
+        }
+    }
+
+    public void transcription(string[] value)
+    {
+        Debug.Log(value[0]);
+        textmesh.GetComponent<TextMeshPro>().text = value[0];
+
+        if (value[0] == "fireball" || value[0] == "Fireball")
+        {
+            Debug.Log("bawambabanabam. woohoo!!");
+        }
+    }
+
+
+    // this is not finding any collision. How the heck do I find a button input
+    public void OnCollisionEnter(Collision cauldCollis)
+    {
+        wit.Activate();
+        Debug.Log("Entered");
+    }
+}
